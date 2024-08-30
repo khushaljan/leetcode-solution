25ecf891dcbfd6cd2a3cc31c563391ee0185c334
khushaljan <138130710+khushaljan@users.noreply.github.com>
2024-08-30 17:58:33 +0530

    Time: 44 ms (66.88%), Space: 27.2 MB (17.55%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-     
-       int j=0;
-       int k=10;
-       int n=s.size();
-       unordered_map<string,int> umap;
-       vector<string> ans;
-         if(n<k){
-                 return ans;
-         }
-        while(j<n-9){
-              
-              string substring=s.substr(j,10);
-              
-             
+        int j=0;
+        unordered_map<string,int> umap;
+        vector<string> ans;
+            int k=10;
+            int n=s.size();
+            // if(n<k){
+            //         return ans;
+            // }
+         while(j<n){
+        string substring=s.substr(j,10);
+        
                 
-                    if(umap.count(substring)){
-                      if(umap[substring]==1){
-                              ans.push_back(substring);
-                              
-                      }
-                    }
-                     umap[substring]++;
-                      if(umap[substring]==0){
-                              umap.erase(substring);
-                      }
-                  
-                  
-                      j++;
-                      
-              
-                
-        }
-           return ans;
+                   if(umap.count(substring)){
+                           if(umap[substring]==1){
+                                   ans.push_back(substring);
+                           }
+                           
+                   }
+                         umap[substring]++;  
+                        
+                         
+
+                         j++;
+                 }
+         
+            return ans;
     }
 };
