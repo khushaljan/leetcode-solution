bb428f1f7a6fd90bb21301d6a9eb977f21172616
khushaljan <138130710+khushaljan@users.noreply.github.com>
2024-10-15 21:08:52 +0530

    Time: 0 ms (100.00%), Space: 8 MB (20.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
         int f(int i,int j,int m,int n,vector<vector<int>>& dp){
            
-            if (i == m - 1 && j == n - 1) {
+              if (i == m - 1 && j == n - 1) {
             return 1;
         }
         // If we go out of bounds, return 0 (no valid path)
-        if (i >= m || j >= n) {
-            return 0;
-        }
-        // Check if the current state is already computed
-        if (dp[i][j] != -1) {
+       if(i>=m || j>=n){
+               return 0;
+       }
+                if(dp[i][j] != -1){
             return dp[i][j];
         }
-
-        // Recursive calls: move down and right
-        int down = f(i + 1, j, m, n, dp);
-        int right = f(i, j + 1, m, n, dp);
-
-        // Store the computed result in dp array and return it
-        
-
-        return dp[i][j]=down+right;
                 
+                    int down = f(i + 1, j, m, n, dp);
+        int right = f(i, j + 1, m, n, dp);
+                
+                  return dp[i][j]=down+right; 
         }
     int uniquePaths(int m, int n) {
 //         m rows and n columns ka grid
