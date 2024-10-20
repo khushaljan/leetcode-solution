3e2b6df317a36aebeec60d582f7aaaa2e56c1d8d
khushaljan <138130710+khushaljan@users.noreply.github.com>
2024-10-20 09:34:10 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (89.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,14 +6,14 @@ public:
     vector<int> prev(n, 0);
 
     // Iterate through the rows of the grid.
-    for (int i = 0; i < m; i++) {
+    for (int i=m-1;i>=0;i--) {
         // Create a temporary vector to represent the current row.
         vector<int> temp(n, 0);
 
         // Iterate through the columns of the grid.
-        for (int j = 0; j < n; j++) {
+        for (int j = n-1; j >=0; j--) {
             // Base case: If we are at the top-left cell (0, 0), there is one way.
-            if (i == 0 && j == 0) {
+            if (i == m-1 && j == n-1) {
                 temp[j] = 1;
                 continue;
             }
@@ -23,12 +23,12 @@ public:
             int left = 0;
 
             // If we are not at the first row (i > 0), update 'up' with the value from the previous row.
-            if (i > 0)
+            if (i <m-1)
                 up = prev[j];
 
             // If we are not at the first column (j > 0), update 'left' with the value from the current row.
-            if (j > 0)
-                left = temp[j - 1];
+            if (j < n-1)
+                left = temp[j + 1];
 
             // Calculate the number of ways to reach the current cell by adding 'up' and 'left'.
             temp[j] = up + left;
@@ -39,7 +39,7 @@ public:
     }
 
     // The result is stored in the last cell of the previous row (n-1).
-    return prev[n - 1];
+    return prev[0];
 }
         
            
