a588139f1be543dc2b81be9630908763e753f242
khushaljan <138130710+khushaljan@users.noreply.github.com>
2024-11-02 13:58:01 +0530

    Time: 29 ms (70.74%), Space: 25.7 MB (61.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-        int distinctsubsequence(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-                 if(i<0 && j>=0){
-                    return 0;
+        
+        int distinctsubsequence(int m,int n,string& s,string& t,vector<vector<int>>& dp){
+                
+               const int MOD=1e9+7;
+                
+                for(int i=0;i<=m;i++){
+                        dp[i][0]=1;
                 }
-                if(j<0){
-                        return 1;
+                
+                for(int j=1;j<=n;j++){
+                        dp[0][j]=0;
                 }
-                if(dp[i][j]!=-1){
-                        return dp[i][j];
-                }
-                if(s[i]==t[j]){
-                return dp[i][j]=distinctsubsequence(i-1,j-1,s,t,dp)+distinctsubsequence(i-1,j,s,t,dp);
+               for(int i=1;i<=m;i++){
+                       for(int j=1;j<=n;j++){
+               
+                if(s[i-1]==t[j-1]){
+                dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%MOD;
                 }
                 else{
-                return dp[i][j]=distinctsubsequence(i-1,j,s,t,dp);
+                dp[i][j]=(dp[i-1][j])%MOD;
                 }
-                
-                
+                       }
+               }
+                return dp[m][n];
         }
     int numDistinct(string s, string t) {
         int m=s.size();
         int n=t.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return distinctsubsequence(m-1,n-1,s,t,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return distinctsubsequence(m,n,s,t,dp);
     }
 };
